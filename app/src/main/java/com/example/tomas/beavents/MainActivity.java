2f56b943495423ec5325e5b95c0410f267837d23
TomasCalderon <tomascg@mit.edu>
2015-03-30 06:05:12 -0400

    The app is fetching images from the filesystem and displaing them!!.

--- app/src/main/java/com/example/tomas/beavents/MainActivity.java
+++ app/src/main/java/com/example/tomas/beavents/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.tomas.beavents;
 
 
 import android.os.Bundle;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +13,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,12 +22,20 @@ import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
-
+import android.widget.TextView;
 
 
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 
 public class MainActivity extends BaseActivity {
@@ -42,6 +56,10 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
         super.onCreateDrawer();
 
+        StrictMode.enableDefaults(); //STRICT MODE ENABLED
+
+        getData();
+
         imageLoader = ImageLoader.getInstance();
         imageLoader.init(ImageLoaderConfiguration.createDefault(this));
 
@@ -57,16 +75,68 @@ public class MainActivity extends BaseActivity {
 
         final GridView gridview = (GridView) findViewById(R.id.gridview);
         gridview.setAdapter(new ImageAdapter());
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-                gridview.getAdapter().getItem(position);
+        //Block of code to make events clickable.
+//        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+//                gridview.getAdapter().getItem(position);
 //                Intent intent = new Intent(MainActivity.this, ActivityTwo.class);
 //                intent.putExtra("position", position);
 //                startActivity(intent);
+            //}
+//        });
+    }
+
+    public void getData(){
+        String result = "";
+        InputStream isr = null;
+        try{
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost("http://18.111.103.177/testdatabase/getAllCustomers.php"); //YOUR PHP SCRIPT ADDRESS
+            HttpResponse response = httpclient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+            isr = entity.getContent();
+        }
+        catch(Exception e){
+            Log.e("log_tag", "Error in http connection " + e.toString());
+        }
+        //convert response to string
+        try{
+            BufferedReader reader = new BufferedReader(new InputStreamReader(isr,"iso-8859-1"),8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
             }
-        });
+            isr.close();
+
+            result=sb.toString();
+        }
+        catch(Exception e){
+            Log.e("log_tag", "Error  converting result "+e.toString());
+        }
+
+        //parse json data
+        try {
+            String s = "";
+            JSONArray jArray = new JSONArray(result);
+
+            for(int i=0; i<jArray.length();i++){
+                JSONObject json = jArray.getJSONObject(i);
+                s = json.getString("FirstName");
+                Log.e("Ojala",s);
+                imagePaths.add(s);
+            }
+
+
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            Log.e("log_tag", "Error Parsing Data "+e.toString());
+        }
+
     }
 
 
@@ -97,6 +167,7 @@ public class MainActivity extends BaseActivity {
                             ViewGroup parent) {
             View view = convertView;
             final ViewHolder gridViewImageHolder;
+            TextView name;
 //            check to see if we have a view
             if (convertView == null) {
 //                no view - so create a new one
@@ -106,10 +177,15 @@ public class MainActivity extends BaseActivity {
                 gridViewImageHolder.imageView.setMaxHeight(80);
                 gridViewImageHolder.imageView.setMaxWidth(80);
                 view.setTag(gridViewImageHolder);
+
+
             } else {
 //                we've got a view
                 gridViewImageHolder = (ViewHolder) view.getTag();
             }
+
+
+
             imageLoader.displayImage("http://18.111.103.177/images/" + mThumbIds[position]
                     , gridViewImageHolder.imageView
                     , options);
@@ -118,110 +194,4 @@ public class MainActivity extends BaseActivity {
         }
     }
 
-
-//        setTitle("Home");
-//        mMenuOptions = getResources().getStringArray(R.array.menu);
-//        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        mDrawerList = (ListView) findViewById(R.id.left_drawer);
-//
-//        // Set the adapter for the list view
-//        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
-//        mDrawerList.setAdapter(new ArrayAdapter<String>(this,R.layout.categories_list_layout,
-//                mMenuOptions));
-//        // Set the list's click listener
-//        mDrawerList.setOnItemClickListener(this);
-//
-//        getSupportActionBar().setHomeButtonEnabled(true);
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//
-//        mDrawerToggle = new ActionBarDrawerToggle(
-//                this,                  /* host Activity */
-//                mDrawerLayout,         /* DrawerLayout object */
-//                R.drawable.ic_drawer,  /* nav drawer image to replace 'Up' caret */
-//                R.string.drawer_open,  /* "open drawer" description for accessibility */
-//                R.string.drawer_close  /* "close drawer" description for accessibility */
-//        ) {
-//            public void onDrawerClosed(View view) {
-//                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-//            }
-//
-//            public void onDrawerOpened(View drawerView) {
-//                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
-//            }
-//        };
-//        mDrawerLayout.setDrawerListener(mDrawerToggle);
-//
-//        Intent intent = getIntent();
-//        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
-//            String query = intent.getStringExtra(SearchManager.QUERY);
-//            Log.d("errorMain",query);
-//        }
-//
-//    }
-//
-//    @Override
-//    protected void onPostCreate(Bundle savedInstanceState) {
-//        super.onPostCreate(savedInstanceState);
-//        // Sync the toggle state after onRestoreInstanceState has occurred.
-//        mDrawerToggle.syncState();
-//    }
-//
-//    @Override
-//    public void onConfigurationChanged(Configuration newConfig) {
-//        super.onConfigurationChanged(newConfig);
-//        // Pass any configuration change to the drawer toggls
-//        mDrawerToggle.onConfigurationChanged(newConfig);
-//    }
-//
-//
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        MenuInflater inflater = getMenuInflater();
-//        inflater.inflate(R.menu.options_menu, menu);
-//
-//        SearchManager searchManager =
-//                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
-//        SearchView searchView =
-//                (SearchView) menu.findItem(R.id.search).getActionView();
-//        ComponentName cn = new ComponentName(this,SearchResultsActivity.class);
-//        searchView.setSearchableInfo(
-//                searchManager.getSearchableInfo(cn));
-//
-//        return super.onCreateOptionsMenu(menu);
-//    }
-//
-//    @Override
-//    public boolean onPrepareOptionsMenu(Menu menu) {
-//        // If the nav drawer is open, hide action items related to the content view
-//        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
-//
-//        return super.onPrepareOptionsMenu(menu);
-//    }
-//
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        if (mDrawerToggle.onOptionsItemSelected(item)) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
-//
-//    @Override
-//    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//        mDrawerList.setItemChecked(position, true);
-//        Intent intent = new Intent(this, CreateEventsActivity.class);
-//        startActivity(intent);
-//        mDrawerLayout.closeDrawer(mDrawerList);
-//    }
-//
-//    @Override
-//    public void setTitle(CharSequence title) {
-//        getSupportActionBar().setTitle(title);
-//    }
 }
