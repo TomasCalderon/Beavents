e8fc1dfb4895989d15a60bb828ea79a7702b8e41
Katherine Wu <kaixi@mit.edu>
2015-04-04 13:13:17 -0400

    status page

--- app/src/main/java/com/example/tomas/beavents/SettingsActivity.java
+++ app/src/main/java/com/example/tomas/beavents/SettingsActivity.java
@@ -1,15 +1,55 @@
 package com.example.tomas.beavents;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 
 /**
  * Created by LLP-student on 3/26/2015.
  */
-public class SettingsActivity extends BaseActivity {
+public class SettingsActivity extends BaseActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
         super.onCreateDrawer();
+
+        // Display the fragment as the main content.
+        FragmentManager mFragmentManager = getFragmentManager();
+        FragmentTransaction mFragmentTransaction = mFragmentManager
+                .beginTransaction();
+        PrefsFragment mPrefsFragment = new PrefsFragment();
+        mFragmentTransaction.replace(android.R.id.content, mPrefsFragment);
+        mFragmentTransaction.commit();
+
+        SharedPreferences.OnSharedPreferenceChangeListener listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+            public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+                System.out.println(key);
+                System.out.println(prefs.getAll());
+                // TODO: Change arrays.xml here
+            }
+        };
+        SharedPreferences prefs =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        prefs.registerOnSharedPreferenceChangeListener(listener);
     }
+
+
+    public static class PrefsFragment extends PreferenceFragment  {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.preferences);
+        }
+
+
+    }
+
+
 }
