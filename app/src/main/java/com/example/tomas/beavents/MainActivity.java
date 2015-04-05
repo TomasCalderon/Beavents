49c1bff880134964b495278ba5def42b477e6f84
David Pineiro <pineiro@mit.edu>
2015-04-05 00:03:42 -0400

    More changes were made to CreateEventsActivity.

--- app/src/main/java/com/example/tomas/beavents/MainActivity.java
+++ app/src/main/java/com/example/tomas/beavents/MainActivity.java
@@ -135,11 +135,7 @@ public class MainActivity extends BaseActivity {
         InputStream isr = null;
         try{
             HttpClient httpclient = new DefaultHttpClient();
-<<<<<<< HEAD
-            HttpPost httppost = new HttpPost("http://18.189.102.74/testdatabase/getAllCustomers.php"); //YOUR PHP SCRIPT ADDRESS
-=======
             HttpPost httppost = new HttpPost(IP_ADRRESS+"testdatabase/getAllCustomers.php"); //YOUR PHP SCRIPT ADDRESS
->>>>>>> 2f6a66241ca8716a363bdfb9fe22da77cc99e78f
             HttpResponse response = httpclient.execute(httppost);
             HttpEntity entity = response.getEntity();
             isr = entity.getContent();
@@ -228,13 +224,7 @@ public class MainActivity extends BaseActivity {
                 gridViewImageHolder = (ViewHolder) view.getTag();
             }
 
-
-
-<<<<<<< HEAD
-            imageLoader.displayImage("http://18.189.102.74/images/" + imagePaths.get(position)
-=======
             imageLoader.displayImage(IP_ADRRESS + "images/" + imagePaths.get(position)
->>>>>>> 2f6a66241ca8716a363bdfb9fe22da77cc99e78f
                     , gridViewImageHolder.imageView
                     , options);
 
